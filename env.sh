dae0ac04d3525132fbb1ba94456b31c61061de60
Hanxing Yang <nighca@live.cn>
2012-09-13 16:40:59 +0800

    add env.sh & modified README

---
+++ env.sh
@@ -0,0 +1,4 @@
+export GOPATH=$GOPATH:$QINIUROOT/gobook/chapter1/calcproj
+export GOPATH=$GOPATH:$QINIUROOT/gobook/chapter2/sorter
+export PATH=$PATH:$QINIUROOT/gobook/chapter1/calcproj/bin
+export PATH=$PATH:$QINIUROOT/gobook/chapter2/sorter/bin
